7c152228f584edb1628fe3a80dec272bdc55b6f3
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-10-23 11:07:28 -0400

    Introduccion Metodos

---
+++ metodos.java
@@ -0,0 +1,26 @@
+public class metodos {
+    //Declararcion de un Metodo
+    //nombreMetodo(parametros){
+    // Cuerpo del metodo   
+    //}
+    //Declarion de un metodo ("SUMAR(a,b)")
+        public int sumar(int a, int b){
+            int resultado=a+b;
+            return resultado;
+        } 
+
+    //Metodo sin retorno de valor
+    public void bienvenida(){
+        System.out.println("Hola bienvenido");
+    }
+
+    //Llamar o utilizar el metodo
+    int suma=sumar(8, 15);
+    int a=sumar(6, 7);
+
+    public static void main(String[]args){
+    
+        
+    }
+   
+}
