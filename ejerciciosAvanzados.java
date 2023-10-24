8a373b37c7201482023b829df06ab507434c1041
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-10-24 11:17:39 -0400

    Ejercicios Avanzados

---
+++ ejerciciosAvanzados.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ejerciciosAvanzados {
+    public static void main(String[]args){
+        //Condicionales - Metodos
+        //Ejercicio 1- Calcular el Factorial
+        System.out.println("Ejercicio 1- Calcular el Factorial");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Ingrese un numero: ");
+        int numero=scanner.nextInt();
+        int calculofactorial=factorial(numero);
+        System.out.println("El factorial de "+numero+" es "+calculofactorial);
+
+        //Bucles - Metodos - Condicionales
+        //Ejercicio 2- Obtener le mayor menor de una lista
+        System.out.println("Ejercicio 2- Obtener le mayor menor de una lista");
+        System.out.print("Ingrese la cantidad de numero: ");
+        int cantidad=scanner.nextInt();
+        int mayor=Integer.MIN_VALUE;
+        int menor=Integer.MAX_VALUE;
+
+        //AGREGAR DATOS A LA LISTA - BUCLES FOR
+        for(int i=0;i<cantidad;i++){
+            System.out.print("Ingrese un numero: ");
+            int numero1=scanner.nextInt();
+            //Condicional para determinar el mayor
+            if(numero1>mayor){
+                mayor=numero1;
+            }
+            if(numero1<menor){
+                menor=numero1;
+            }
+        }
+        System.out.println("El numero mayor es: "+ mayor);
+        System.out.println("El numero menor es: "+ menor);
+        
+    }
+        //Condicionales - Metodos
+        //Ejercicio 1- Calcular el Factorial
+    public static int factorial(int n){
+        if (n==0 || n==1){
+            return 1;
+        }else{
+            return n*factorial(n-1);
+        }
+    }
+    //Bucles - Metodos - Condicionales
+    //Ejercicio 2- Obtener le mayor menor de una lista
+}
